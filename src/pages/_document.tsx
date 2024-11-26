f2feacc12a66c21d0f9638d91b43df180715cd96
d2-core <dohaktae88@gmail.com>
2024-11-26 16:59:45 +0900

    document root portal 추가

---
+++ src/pages/_document.tsx
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+function Document() {
+  return (
+    <Html>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+        <div id="root-portal" />
+      </body>
+    </Html>
+  )
+}
+
+export default Document
