084bcd925e516794113c66eb355a3f91989b931e
d2-core <dohaktae88@gmail.com>
2024-11-28 16:46:47 +0900

    CORS error 경로 우회 설정 추가

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   swcMinify: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*', // 들어오는 요청 경로 패턴
+        destination: 'http://localhost:8080/:path*', // 라우팅하려는 경로
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
