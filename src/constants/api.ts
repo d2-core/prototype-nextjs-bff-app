35ab62558c5155c763fecf1f151aa156bb049905
d2-core <dohaktae88@gmail.com>
2024-12-01 23:43:11 +0900

    gateway predicates 반영

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -2,10 +2,10 @@ export const API = {
   OK: 'OK',
 }
 export const DOMAIN = {
-  AUTH: '/auth/',
-  EVENT: '/event/',
-  NOTIFICATION: '/notification/',
-  PRODUCT: '/product/',
+  AUTH: '/auth-service/',
+  EVENT: '/event-service/',
+  NOTIFICATION: '/notification-service/',
+  PRODUCT: '/product-service/',
 }
 
 export type Domain = keyof typeof DOMAIN
