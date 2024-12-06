d975afd43592bb107f9383eaab4e7c5e717caf6a
d2-core <dohaktae88@gmail.com>
2024-12-06 16:36:11 +0900

    User 초기화 로직 최종

--- src/components/init/UserInit.tsx
+++ src/components/init/UserInit.tsx
@@ -1,4 +1,5 @@
 import { LOCALSTORAGE } from '@/constants/localstorage'
+import useUser from '@/hooks/auth/useUser'
 import { getAppUser } from '@/remote/api/auth'
 import { userAtom } from '@/store/atom/user'
 import { authStorage as authLocalStorage } from '@/store/local'
@@ -6,31 +7,24 @@ import { useEffect, useState } from 'react'
 import { useQuery } from 'react-query'
 import { useSetRecoilState } from 'recoil'
 
-function InitGuard({ children }: { children: React.ReactNode }) {
-  const [init, setInit] = useState(false)
+function UserInit({ children }: { children: React.ReactNode }) {
   const [accessToken, setAccessToken] = useState<string | null>(null)
-
+  const user = useUser()
   const setUser = useSetRecoilState(userAtom)
-  const { data } = useQuery(['user'], () => getAppUser(), {
-    enabled: !!accessToken,
-    suspense: true,
+
+  const { data } = useQuery(['user'], getAppUser, {
+    enabled: Boolean(accessToken) && !Boolean(user),
   })
 
   useEffect(() => {
-    const token = authLocalStorage.get(LOCALSTORAGE.AUTH.ACESS_TOKEN)
+    const token = authLocalStorage.get(LOCALSTORAGE.AUTH.ACCESS_TOKEN)
     setAccessToken(token)
     if (data?.body) {
       setUser(data.body)
     }
-
-    setInit(true)
   }, [data, setUser])
-
-  if (init === false) {
-    return null
-  }
 
   return <>{children}</>
 }
 
-export default InitGuard
+export default UserInit
