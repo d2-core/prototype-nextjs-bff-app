5841b397c01ef13586c006c2a1dd5fb09404e3fb
d2-core <dohaktae88@gmail.com>
2024-12-08 23:40:15 +0900

    Select, TextFiled Form Shared Component 추가

---
+++ src/components/shared/Select.tsx
@@ -0,0 +1,57 @@
+import { forwardRef, SelectHTMLAttributes } from 'react'
+
+import Flex from './Flex'
+import Text from './Text'
+import styled from '@emotion/styled'
+import { colors } from '@styles/colorPalette'
+
+export interface Option {
+  label: string
+  value: string | number | undefined
+}
+
+interface SelectProps extends SelectHTMLAttributes<HTMLSelectElement> {
+  label?: string
+  options: Option[]
+}
+
+const BaseSelect = styled.select`
+  height: 52px;
+  border: 1px solid ${colors.gray300};
+  border-radius: 6px;
+  padding: 0 16px;
+  cursor: pointer;
+
+  &:required:invalid {
+    color: #c0c4c7;
+  }
+`
+
+const Select = forwardRef<HTMLSelectElement, SelectProps>(function Select(
+  { label, options, value, ...props },
+  ref,
+) {
+  return (
+    <Flex direction="column">
+      {label ? (
+        <Text
+          typography="t7"
+          color="black"
+          display="inline-block"
+          style={{ marginBottom: 6 }}
+        >
+          {label}
+        </Text>
+      ) : null}
+      <BaseSelect required={true} ref={ref} value={value} {...props}>
+        {options.map(({ label, value }) => (
+          <option key={label} value={value}>
+            {label}
+          </option>
+        ))}
+      </BaseSelect>
+    </Flex>
+  )
+})
+
+export default Select
