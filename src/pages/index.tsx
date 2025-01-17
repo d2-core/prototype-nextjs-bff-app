a04db5d560753301f5f90800347b51ed900a8fb5
d2-core <dohaktae88@gmail.com>
2025-01-17 21:35:24 +0900

    homepage template 구현

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,14 @@
+import AppIntroduce from '@/components/introdution/AppIntrodution'
+import TopNav from '@/components/shared/TopNav'
+import { Box } from '@mui/material'
+
 function Home() {
-  return <div>Hello</div>
+  return (
+    <Box>
+      <TopNav />
+      <AppIntroduce />
+    </Box>
+  )
 }
 
 export default Home
