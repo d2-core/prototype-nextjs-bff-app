4a8080c709444f355f42a5d2fe3d40de79435fbb
d2-core <dohaktae88@gmail.com>
2025-01-17 21:39:06 +0900

    withAuthGuard hoc 구현

--- src/components/shared/hocs/hocs/withAuthGuard.tsx
+++ src/components/shared/hocs/hocs/withAuthGuard.tsx
@@ -4,41 +4,38 @@ import useUser from '@/hooks/useUser'
 
 function withAuthGuard(Component: React.ComponentType) {
   const AuthenticatedComponent = (props: any) => {
-    const { user } = useUser()
-    const router = useRouter()
-    const [isAuthenticated, setIsAuthenticated] = useState<boolean | null>(null)
+    // const { user } = useUser()
+    // const router = useRouter()
+    // const [isAuthenticated, setIsAuthenticated] = useState<boolean | null>(null)
+    // useEffect(() => {
+    //   const publicRoutes = ['/', '/auth/login', '/auth/signup']
+    //   if (!publicRoutes.includes(router.pathname) && user == null) {
+    //     setIsAuthenticated(false)
+    //     router.push('/auth/login')
+    //   } else {
+    //     setIsAuthenticated(true)
+    //   }
+    // }, [router, user])
+    // if (isAuthenticated === null) {
+    //   return (
+    //     <div
+    //       style={{
+    //         display: 'flex',
+    //         justifyContent: 'center',
+    //         alignItems: 'center',
+    //         height: '100vh',
+    //       }}
+    //     >
+    //       <p>Loading...</p>
+    //     </div>
+    //   )
+    // }
+    // if (isAuthenticated) {
+    //   return <Component {...props} />
+    // }
+    // return null
 
-    useEffect(() => {
-      const publicRoutes = ['/', '/auth/login', '/auth/signup']
-
-      if (!publicRoutes.includes(router.pathname) && user == null) {
-        setIsAuthenticated(false)
-        router.push('/auth/login')
-      } else {
-        setIsAuthenticated(true)
-      }
-    }, [router, user])
-
-    if (isAuthenticated === null) {
-      return (
-        <div
-          style={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            height: '100vh',
-          }}
-        >
-          <p>Loading...</p>
-        </div>
-      )
-    }
-
-    if (isAuthenticated) {
-      return <Component {...props} />
-    }
-
-    return null
+    return <Component {...props} />
   }
 
   return AuthenticatedComponent
