0274828c8ce31f537c9d7115ee5fbed67d3a30df
d2-core <dohaktae88@gmail.com>
2025-01-21 21:31:46 +0900

    ListDirection 디자인 변경

--- src/components/shared/ListDirection.tsx
+++ src/components/shared/ListDirection.tsx
@@ -1,16 +1,20 @@
 import { Box, Button, Typography } from '@mui/material'
 import { useRouter } from 'next/router'
 import { ReactNode } from 'react'
+import Spacing from './Spacing'
 
 interface Props {
   title: ReactNode
-  routePath: string
+  length: number
+  routePath?: string
 }
 
-function ListDirection({ title, routePath }: Props) {
+function ListDirection({ title, length, routePath }: Props) {
   const route = useRouter()
   const handleRoutePath = () => {
-    route.push(routePath)
+    if (routePath) {
+      route.push(routePath)
+    }
   }
   return (
     <Box
@@ -19,10 +23,20 @@ function ListDirection({ title, routePath }: Props) {
       alignItems="center"
       mb={2}
     >
-      <Typography variant="h6">{title}</Typography>
-      <Button variant="text" onClick={handleRoutePath}>
-        더보기
-      </Button>
+      <Box display={'flex'} alignItems={'center'}>
+        <Typography variant="h6">{title}</Typography>
+        {length !== 0 && (
+          <Typography component="span" color="text.secondary" sx={{ ml: 1 }}>
+            ({length})
+          </Typography>
+        )}
+      </Box>
+      <Spacing />
+      {routePath && (
+        <Button variant="text" onClick={handleRoutePath}>
+          더보기
+        </Button>
+      )}
     </Box>
   )
 }
